afc477173f88984fb3a2ca9130d9d64fe2bc7805
zhousiwei <2434387555@qq.com>
2020-02-28 11:00:08 +0800

    Updated

--- src/test/java/io/github/service/SysUserServiceTest.java
+++ src/test/java/io/github/service/SysUserServiceTest.java
@@ -8,25 +8,24 @@ import java.util.List;
 
 /**
  * 系统用户测试
- * 
+ *
  * @author Joey
  * @Email 2434387555@qq.com
- *
  */
 public class SysUserServiceTest extends BaseAppTest {
 
-	@Resource
-	private SysUserService sysUserService;
+    @Resource
+    private SysUserService sysUserService;
 
-	/**
-	 * 查询列表
-	 */
-	//@Test
-	public void selectList() {
-		List<SysUserEntity> selectList = sysUserService.selectList(null);
-		for (SysUserEntity sysUserEntity : selectList) {
-			System.out.println(sysUserEntity);
-		}
-	}
+    /**
+     * 查询列表
+     */
+    //@Test
+    public void selectList() {
+        List<SysUserEntity> selectList = sysUserService.selectList(null);
+        for (SysUserEntity sysUserEntity : selectList) {
+            System.out.println(sysUserEntity);
+        }
+    }
 
 }
